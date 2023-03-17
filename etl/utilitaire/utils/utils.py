0b1fc86e0496c8e8a2d79eb93fdd28676a54942d
beatricedaniel <beatrice.yk.daniel@protonmail.com>
2023-03-17 14:38:26 +0000

    MAJ 17/03

--- etl/utilitaire/utils/utils.py
+++ etl/utilitaire/utils/utils.py
@@ -4,6 +4,11 @@
 import json
 import logging
 import os
+import pandas as pd
+import re
+
+from unidecode import unidecode
+
 
 def read_settings(path_in, dict, elem):
      """
@@ -24,3 +29,65 @@ def read_settings(path_in, dict, elem):
                param_config = param.copy()
      logging.info("Lecture param config " + path_in + ".")
      return param_config 
+
+
+
+def checkIfPathExists(file):
+     """
+     Permet de vérifier le fichier 
+
+     Paramètre :
+     - file : Fichier à vérifier
+     """
+     if os.path.exists(file):
+          os.remove(file)
+          print("Ancier fichier écrasé")
+
+
+
+def convertXLSXtoCSV(inputExcelFilePath, outputCsvFilePath):
+     """
+     Permet de convertir les fichiers XLSX en fichier Excel
+
+     inputExcelFIlePath = 
+     outputCsvFilePath = 
+     """
+
+     try:
+          excelFile = pd.read_excel(inputExcelFilePath, header = 0)
+          checkIfPathExists(outputCsvFilePath)
+
+          excelFile.to_csv(outputCsvFilePath, index = None, header = True, sep = ";", encoding = 'UTF-8')
+          return outputCsvFilePath
+     
+     except ValueError as err:
+          print(err)
+          return str(err)
+
+
+def cleanTxt(text):
+     """
+
+     """
+
+     try:
+          text = unicode(text.lower(), 'utf-8')
+     except (TypeError, NameError):
+               pass
+
+     text = unicode(text.lower())
+     text = text.encode('ascii', 'ignore')
+     text = text.decode('utf-8')
+
+     text = re.sub('[] +', '_', text)
+     text = re.sub('[^0-9a-zA-Z_-]', '',text) 
+     return str(text)
+
+
+def cleanSrcData(df):
+     """
+     Permet d'enlever les caractères spéciaux, accents, espace (_)
+     """
+
+     df.columns = [cleanTxt(i) for i in df.columns.values.tolist()]
+     return df
